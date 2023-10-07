cbb08c98c00888a4f534256a687e02c76d6d5946
Christian CoC <christianelmillo81@gmail.com>
2023-10-07 02:51:23 -0300

    Commit part1-bootcamp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Componentes Multiples
+const Mensaje = () => {
+  return  <h1>Este es el mensaje</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Description = () => { 
+  return (
+    <div>
+      <p>Esta es la descripción</p>
+    </div>
+  ) 
+  
+}
+// No es de buena práctica poner más de un componente.
+
+// Componentes con parámetros
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Pintado = (props) => {
+  return (
+    <div>
+      <h2 style={{ color:props.color }}>El color es {props.pintura}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Mensaje />
+      <Description />
+      <Hello name="Juan" age={26 + 10} />
+      <Pintado color='red' pintura='Rojo'/>
+    </div>
+      
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root')) 
